daf46c7b0867ffc4c018a7d39d6b6bdddc806e18
Mohamed Almahdi <almahdimohamed627@gmail.com>
2025-11-13 13:58:18 +0300

    fixed product image

--- backend/database/migrations/2025_11_09_112452_create_products_table.php
+++ backend/database/migrations/2025_11_09_112452_create_products_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
-            $table->integer('stock')->default(0);
-            $table->string('image_url')->nullable();
+            $table->string('image')->nullable();
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
