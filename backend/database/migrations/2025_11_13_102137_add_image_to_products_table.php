2c6e48bed74115d5600ec81d5d46b6b74ae3cccb
Mohamed Almahdi <almahdimohamed627@gmail.com>
2025-11-13 13:51:47 +0300

    fixed product image

--- backend/database/migrations/2025_11_13_102137_add_image_to_products_table.php
+++ backend/database/migrations/2025_11_13_102137_add_image_to_products_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        // In the 'up()' method
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('image')->nullable(); // Stores the file path
         });
     }
 
